1b5436ec5513107e224fdd9f30e5c4767a8f84a8
Juanma <Juanma@JuanmaElCano>
2018-02-09 14:44:41 +0100

    09/02/2018 crear asteroide y su angulo aleatorio

--- src/asteroides/Asteroides.java
+++ src/asteroides/Asteroides.java
@@ -28,39 +28,45 @@ import javafx.stage.Stage;
 public class Asteroides extends Application {
     //Nave
     Polygon nave = new Polygon ();
-    //variable angulo
-    double angulo;
+    //Asteroide
+    Polygon aster = new Polygon ();
+    //variable angulo de la nave
+    double NavAngulo;
     //variable direccion (aplicado de 0 a 359 grados)
     double direccion;
     //variable direccion convertida en radianes
     double direccionRAD;
     //variable direccion X aplicando seno del angulo
-    double dirX;
+    double NavDirX;
     //variable direccion Y aplicando coseno del angulo
-    double dirY;
+    double NavDirY;
     //variable de velocidad de la nave
+    double NavVelX;
+    double NavVelY;
+   
     double velNave;
     //variable de velocidad de giro
-    int velGiro;
+    int NavVelGiro;
     //Variables de la posicion nave
     double posX = 400;
     double posY = 200;
+    //
+    double AstAngulo = Math.random()*359;
     
           
     //Variables ventana
-    final int ventanax =800;
-    final int ventanay =400;
+    final int ventanaX =1300;
+    final int ventanaY =720;
     Scene ventana;
     
     
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
-        ventana = new Scene(root, ventanax, ventanay, Color.WHITE);
+        ventana = new Scene(root, ventanaX, ventanaY, Color.WHITE);
         primaryStage.setScene(ventana);
         primaryStage.show();
-        
-        
+              
         nave.setFill(Color.RED);
         nave.getPoints().addAll(new Double[]{
             0.0, -30.0,
@@ -68,29 +74,44 @@ public class Asteroides extends Application {
             0.0, -5.0,
             -10.0, 0.0});
         root.getChildren().add(nave);
-       
+        
+        aster.setFill(Color.BLUE);
+        aster.getPoints().addAll(new Double[]{
+            0.0, 10.0,
+            40.0, -10.0,
+            100.0, 10.0,
+            110.0, 60.0,
+            50.0, 80.0,
+            -20.0, 50.0
+        });    
+        //muestra el asteroide
+        root.getChildren().add(aster);
+        
+        aster.setLayoutX(100);
+        aster.setLayoutY(20);
+        
         ventana.setOnKeyPressed((KeyEvent event) -> {
             
             switch(event.getCode()){
                 case RIGHT:
-                    velGiro = 2;
+                    NavVelGiro = 2;
                     break;
                 case LEFT:
-                    velGiro = -2;
+                    NavVelGiro = -2;
                     break;
                 case UP:
-                    dirX = Math.sin(direccionRAD);
-                    dirY = Math.cos(direccionRAD);
-                    velNave += 0.1;
-                    if (velNave >= 2) {
-                        velNave = 2;
-                    };
+                    //calcular direccion de la nave
+                    NavDirX = Math.sin(direccionRAD);
+                    NavDirY = Math.cos(direccionRAD);
+                    //calcular la velocidad
+                    NavVelX += (NavDirX * 0.2);
+                    NavVelY += (-NavDirY * 0.2);
                     break;
             }
         });
         
         ventana.setOnKeyReleased((KeyEvent event) -> {
-            velGiro = 0;
+            NavVelGiro = 0;
         });
 
         //Llamada a la animación
@@ -99,22 +120,33 @@ public class Asteroides extends Application {
     
     AnimationTimer animationNave = new AnimationTimer() {
             @Override
-            public void handle(long now) {
-                
-                direccion = angulo % 360;
-                direccionRAD = Math.toRadians(direccion);
-                
-                
-                
-                angulo += velGiro;
-                nave.setRotate(direccion);
-                
-                posX += (dirX * velNave);
-                posY += (-dirY * velNave);
-                
-                nave.setLayoutX(posX);
-                nave.setLayoutY(posY);
-                
+        public void handle(long now) {
+            //la direccion es el resto del angulo entre 360    
+            direccion = NavAngulo % 360;
+            direccionRAD = Math.toRadians(direccion);               
+            //giro de la nave
+            NavAngulo += NavVelGiro;
+            nave.setRotate(direccion);
+            //modificar la posicion en relacion a la velocidad   
+            posX += NavVelX;
+            posY += NavVelY;
+            //para que la nave vuelva a aparecer por los bordes
+            if (posX >= ventanaX){
+                posX = 0;
             }
-        };
-}//Cierre de la Clase JuegoNave
+            if (posY >= ventanaY){
+                posY = 0;
+            }
+            if (posX < 0){
+                posX = ventanaX;
+            }
+            if (posY < 0){
+                posY = ventanaY;
+            }
+            //cambio de direccion de la nave / movimiento
+            nave.setLayoutX(posX);
+            nave.setLayoutY(posY);
+                
+        }
+    };
+}
